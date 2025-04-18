41f9d1e567951ce46e70e20c1a0c2d8e6d9d17db
Asterix522 <ecrotchety2@gmail.com>
2025-04-18 10:19:31 -0700

    monster

--- src/Scenes/Monster.js
+++ src/Scenes/Monster.js
@@ -31,15 +31,64 @@ class Monster extends Phaser.Scene {
         //
         // look in spritesheet_default.xml for the individual sprite names
         // You can also download the asset pack and look in the PNG/default folder.
-        my.sprite.body = this.add.sprite(this.bodyX, this.bodyY, "monsterParts", "body_greenD.png");
+        // Body
+        my.sprite.body = this.add.sprite(this.bodyX, this.bodyY, "monsterParts", "body_redB.png");
 
+        // Legs
+        my.sprite.rightLeg = this.add.sprite(this.bodyX + 100, this.bodyY + 120, "monsterParts", "leg_redB.png");
+        my.sprite.leftLeg = this.add.sprite(this.bodyX - 100, this.bodyY + 120, "monsterParts", "leg_redB.png");
+        my.sprite.leftLeg.setScale(-1, 1); // Flip horizontally
+
+        // Arms
+        my.sprite.rightArm = this.add.sprite(this.bodyX + 100, this.bodyY + 10, "monsterParts", "arm_redB.png");
+        my.sprite.leftArm = this.add.sprite(this.bodyX - 100, this.bodyY + 10, "monsterParts", "arm_redB.png");
+        my.sprite.leftArm.setScale(-1, 1); // Flip horizontally
+
+        // Eyes
+        my.sprite.eye = this.add.sprite(this.bodyX, this.bodyY - 10, "monsterParts", "eye_psycho_light.png");
+
+        // Mouth
+        my.sprite.smile = this.add.sprite(this.bodyX, this.bodyY + 50, "monsterParts", "mouth_closed_happy.png");
+
+        // Fang (hidden by default)
+        my.sprite.fang = this.add.sprite(this.bodyX, this.bodyY + 50, "monsterParts", "mouthI.png");
+        my.sprite.fang.setVisible(false);
+
+        this.keys = this.input.keyboard.addKeys('S,F,A,D'); 
         
     }
 
     update() {
         let my = this.my;    // create an alias to this.my for readability
 
+        if (Phaser.Input.Keyboard.JustDown(this.keys.S)) {
+            my.sprite.smile.setVisible(true);
+            my.sprite.fang.setVisible(false);
+        }
+        if (Phaser.Input.Keyboard.JustDown(this.keys.F)) {
+            my.sprite.smile.setVisible(false);
+            my.sprite.fang.setVisible(true);
+        }
+
+        const moveSpeed = 5;
+        if (this.keys.A.isDown) {
+            this.bodyX -= moveSpeed;
+        }
+        if (this.keys.D.isDown) {
+            this.bodyX += moveSpeed;
+        }
+
+        my.sprite.body.setPosition(this.bodyX, this.bodyY);
+        my.sprite.rightLeg.setPosition(this.bodyX + 100, this.bodyY + 120);
+        my.sprite.leftLeg.setPosition(this.bodyX - 100, this.bodyY + 120);
+        my.sprite.rightArm.setPosition(this.bodyX + 100, this.bodyY + 10);
+        my.sprite.leftArm.setPosition(this.bodyX - 100, this.bodyY + 10);
+        my.sprite.eye.setPosition(this.bodyX, this.bodyY - 10);
+        my.sprite.smile.setPosition(this.bodyX, this.bodyY + 50);
+        my.sprite.fang.setPosition(this.bodyX, this.bodyY + 50);
+
        
     }
+
 
 }
